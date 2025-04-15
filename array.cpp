647d90df86e6f1f56edeb990ec10ab6417535993
Afifah Miftakhul <apipiyahmiftakhul@gmail.com>
2025-04-15 15:15:45 +0700

    deklarasi array

---
+++ array.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace  std;
+
+//dektarasi array tanpa ukuran, tetapi wajib mengisikan data
+int nilai[] = {12,5,9,10,34};
+//deklarasi array dengan ukuran dan diberi nilai awal
+int skor[5] = {18,12,18,6};
+//dektarasi array dengan ukuran dan tidak ada nilai awal 
+string nama [3];
+
+int main(){
+cout << "Skor ke 3 = " << skor[2] << endl;
+//mengisi array pada  lokasi/index tertentu
+skor [2] = 25;
+cout << "Skor ke 3 = " << skor [2] << endl;
+
+//mengisi array dengan loop for
+for(int i = 0; i < 3; i++){
+cout << "Nama Mahasiswa ke-" << i+1 << "= ";
+ cin >> nama [i];
+}
+cout << endl;
+//menampilkan array dengan loop for
+for (int i = 0; i < 3; i++)
+{
+    cout << "Data Mahasiswa ke-" << i+1 << " = " << nama[i]
+    << endl;
+}
+}
