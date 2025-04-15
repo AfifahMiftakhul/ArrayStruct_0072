d8d1c2c82151731fad0a22b7c10e3f825218b124
Afifah Miftakhul <apipiyahmiftakhul@gmail.com>
2025-04-15 15:40:12 +0700

    struct

---
+++ struct.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+struct mahasiswa{
+string nim;
+string nama; 
+string alamat;
+};
+
+int main (){
+    mahasiswa mhs;
+
+    mhs.nim = "20240140001";
+    mhs.nama = "Abra";
+    mhs.alamat = "Kasihan";
+
+    cout << "Masukkan NIM = ";
+    cin >> mhs.nim;
+    cout << "Masukkan Nama = "; 
+    cin >> mhs.nama;
+    cout << "Masukkan Alamat = "; 
+    cin >> mhs.alamat;
+
+    cout << endl;
+    cout << "Data Mahasiswa" << endl;
+
+    cout << "NIM = " << mhs.nim << endl;
+    cout << "Nama = "  << mhs.nama << endl;
+    cout << "Alamat = "  << mhs.alamat << endl;
+
+}
