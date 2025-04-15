44145d73239523c0e4a2b2bc958ad42c3d41eb85
Afifah Miftakhul <apipiyahmiftakhul@gmail.com>
2025-04-15 15:53:51 +0700

    mengisi data mahasiswa

--- struct.cpp
+++ struct.cpp
@@ -1,11 +1,4 @@
-#include <iostream>
-using namespace std;
 
-struct mahasiswa{
-string nim;
-string nama; 
-string alamat;
-};
 
 int main (){
     mahasiswa mhs;
@@ -21,4 +14,11 @@ int main (){
     cout << "Masukkan Alamat = "; 
     cin >> mhs.alamat;
 
-    
+    cout << endl;
+    cout << "Data Mahasiswa" << endl;
+
+    cout << "NIM = " << mhs.nim << endl;
+    cout << "Nama = "  << mhs.nama << endl;
+    cout << "Alamat = "  << mhs.alamat << endl;
+
+}
