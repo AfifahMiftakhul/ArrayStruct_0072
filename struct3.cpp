28834a36562a9b903bc634cdf4ff5f0c799ade0a
Afifah Miftakhul <apipiyahmiftakhul@gmail.com>
2025-04-15 16:35:09 +0700

    data mahasiswa struct  3

--- struct3.cpp
+++ struct3.cpp
@@ -12,18 +12,14 @@ struct mahasiswa{
     detailAlamat alamat;
     };
     
-    int main (){
-        mahasiswa mhs;
+   
     
-        cout << "Masukkan NIM = ";
-        cin >> mhs.nim;
-        cin.ignore ();
-        cout << "Masukkan Nama = "; 
-        getline(cin, mhs.nama);
-        cout << "Masukkan Alamat Desa = "; 
-        cin >> mhs.alamat.desa;
-        cout << "Masukkan Alamat Kota = "; 
-        cin >> mhs.alamat.kota;
+        cout << endl;
+        cout << "Data Mahasiswa" << endl;
     
-    
+        cout << "NIM = " << mhs.nim << endl;
+        cout << "Nama = "  << mhs.nama << endl;
+        cout << "Alamat Desa = "  << mhs.alamat.desa << endl;
+        cout << "Alamat Kota = "  << mhs.alamat.kota << endl;
+    }
         
